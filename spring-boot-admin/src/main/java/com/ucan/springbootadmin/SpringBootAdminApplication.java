64b171aba781b7aaa760ad66fce06b32238852b3
Mandar Dharurkar <mandar.dharurkar@stagrp.com>
2018-02-15 14:14:27 +0530

    initial commit

---
+++ spring-boot-admin/src/main/java/com/ucan/springbootadmin/SpringBootAdminApplication.java
@@ -0,0 +1,16 @@
+package com.ucan.springbootadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+@SpringBootApplication
+@EnableAdminServer
+@EnableDiscoveryClient
+public class SpringBootAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdminApplication.class, args);
+	}
+}
